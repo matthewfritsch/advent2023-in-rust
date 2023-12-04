d85b0a66550b2b7f4dbb3d062ad8e1fe0dc114f2
Matthew Fritsch <matthewmfritsch@gmail.com>
2023-12-03 16:12:38 -0800

    sum of powers. again, more garbage in terms of inefficiencies by not using the same loop for both answers... but I'm starting to get the hang of working with components of the language :)

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,10 +1,24 @@
 use std::fs;
 
+struct GameResults {
+    sum: u32,
+    powers: u32
+}
+
 fn main() {
     let file_contents = fs::read_to_string("input.txt").expect("File not found.");
 
-    let sum = valid_indexes(file_contents);
-    println!("{sum}");
+    let game_results = get_results(file_contents);
+
+    println!("Sum: {}", game_results.sum);
+    println!("Powers: {}", game_results.powers);
+}
+
+fn get_results(file_contents: String) -> GameResults {
+    GameResults {
+        sum: valid_indexes(file_contents.clone()),
+        powers: all_powers_per_game(file_contents.clone()),
+    }
 }
 
 fn valid_indexes(file_contents: String) -> u32 {
@@ -14,13 +28,22 @@ fn valid_indexes(file_contents: String) -> u32 {
     let b_max: u16 = 14;
 
     for line in file_contents.lines() {
-        sum += u32::from(process_game(line, r_max, g_max, b_max));
+        sum += u32::from(valid_game_id(line, r_max, g_max, b_max));
     }
 
     sum
 }
 
-fn process_game(line: &str, r: u16, g: u16, b: u16) -> u16 {
+fn all_powers_per_game(file_contents: String) -> u32 {
+    let mut sum: u32 = 0;
+    for line in file_contents.lines() {
+        sum += min_items_per_game(line);
+    }
+
+    sum
+}
+
+fn valid_game_id(line: &str, r: u16, g: u16, b: u16) -> u16 {
     let game_and_info: Vec<&str> = line.split(':').collect();
     if game_and_info.len() < 2 {
         return 0
@@ -38,6 +61,39 @@ fn process_game(line: &str, r: u16, g: u16, b: u16) -> u16 {
     }
 
     game_number
+}
+
+fn min_items_per_game(game: &str) -> u32 {
+    let mut sums = 0;
+    
+    let game_and_info: Vec<&str> = game.split(':').collect();
+
+    if game_and_info.len() < 2 {
+        return 0
+    }
+
+    let mut min_r = 0;
+    let mut min_g = 0;
+    let mut min_b = 0;
+
+    for stage in game_and_info[1].split(';').collect::<Vec<&str>>() {
+
+        for ball_shown in stage.split(',').collect::<Vec<&str>>() {
+            let qty_and_colour: Vec<&str> = ball_shown.split(' ').collect();
+            let qty = qty_and_colour[1].parse::<u32>().unwrap();
+            let colour = qty_and_colour[2];
+
+            if colour == "red" && qty > min_r {
+                min_r = qty;
+            } else if colour == "green" && qty > min_g {
+                min_g = qty;
+            } else if colour == "blue" && qty > min_b {
+                min_b = qty;
+            }
+        }
+    }
+    sums += (min_r * min_g) * min_b;
+    return sums;
 }
 
 fn stage_is_valid(stage: &str, r: u16, g: u16, b: u16) -> bool {
